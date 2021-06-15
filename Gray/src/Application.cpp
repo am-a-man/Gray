b6a621ffd84603abdfe94d3dea508f0548032c41
Aman Kumar <avy0219@gmail.com>
2021-06-15 15:16:55 +0530

    updating source code and deleting log files

--- Gray/src/Application.cpp
+++ Gray/src/Application.cpp
@@ -5,10 +5,56 @@
 #include<string>
 
 
-
-static int compileShader(const std::string& source, const int& shaderType)
+static std::string vertexSource()
 {
-    
+    return
+        "#version 330 core\n"
+        "layout(location = 0) in vec4 position;\n"
+        "\n"
+        "void main(){\n"
+        "gl_Position = position ;}\n";
+}
+
+
+static std::string fragmentSource()
+{
+    return 
+        "#version 330 core\n"
+        "layout(location = 0) out vec4 color;\n"
+        "\n"
+        "void main(){\n"
+        "color = vec4(1.0 , 0.0 , 0.0 ,1.0);}\n";
+}
+
+static unsigned int compileShader(const int& shaderType, const std::string& source)
+{
+    unsigned int customShader = glCreateShader(shaderType);
+    const char* src = source.c_str();// this can also be written as "&source[0]" this is what I think I would be using most of the time
+    glShaderSource(customShader, 1, &src, NULL);
+    glCompileShader(customShader);
+    int result;
+    glGetShaderiv(customShader, GL_COMPILE_STATUS, &result );
+
+    //TODO : include error hadling: we are going to check what the compile status of glCompileShader is 
+    // now since we can check that glCompileShader's return type is void 
+    // we are going to use 
+    if (result == 0)
+    {
+        int length;
+        glGetShaderiv(customShader, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(customShader, length, &length, message);
+        std::cout << "failed to compile " << ((shaderType == GL_VERTEX_SHADER) ? "VERTEX" : "FRAGMENT") << " shader\n";
+        std::cout << "some error occoured: " <<message<< std::endl;
+        glDeleteShader(customShader);
+    }
+    else 
+    {
+        std::cout<<"everything runs smoothly\n";
+    }
+    // error handling ends
+
+    return customShader;
 }
 
 
@@ -37,9 +83,16 @@ static int createShader(const std::string& vertexShader, const std::string& frag
     */
     
     unsigned int program = glCreateProgram();
-    unsigned int vs = glCreateShader(GL_VERTEX_SHADER);
+    unsigned int vs = compileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = compileShader(GL_FRAGMENT_SHADER, fragmentShader);
     
-    
+    glAttachShader(program, vs); // this is the program created by glCreateProgram() in the above lines of code
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+    glDeleteShader(fs);
+    glDeleteShader(vs);
+    return 1;
 }
 
 
@@ -47,10 +100,7 @@ int main(int argc, char** argv)
 {
     GLFWwindow* window;
 
-    if (glewInit() == GLEW_OK)
-        std::cout << "yay";
-    else
-        std::cout << "nah";
+   
 
     /* Initialize the library */
     if (!glfwInit())
@@ -70,9 +120,9 @@ int main(int argc, char** argv)
     std::cout << glGetString(GL_VERSION) << std::endl;
 
     if (glewInit() == GLEW_OK)
-        std::cout << "yay";
+        std::cout << "Glew is working, all gl function pointers are loaded\n";
     else
-        std::cout << "nah";
+        std::cout << "errors in Glew\n";
     
     unsigned int buffer;
     glGenBuffers(1, &buffer);
@@ -86,6 +136,14 @@ int main(int argc, char** argv)
     glBufferData(GL_ARRAY_BUFFER, 6*sizeof(float),&positions,GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (const void*)0);
+    
+    
+    std::string vertexShader = vertexSource();
+    std::string fragmentShader = fragmentSource();
+    int shader = createShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
+
+
         /*when we actually apply this we define vertex as struct so ultimately we have to give in size of struct in offset*/
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -94,6 +152,7 @@ int main(int argc, char** argv)
         /* Render here */
         try {
             glDrawArrays(GL_TRIANGLES, 0, 3);
+
         }
         catch (const std::exception& error)
         {
